18160a64b43813db075c0adb13547e7f4b7fa7f8
Andrey Manzadey <andrey.manzadey@gmail.com>
2023-09-26 13:37:20 +0300

    delete mixin phpdoc from traits

--- src/Traits/HasDump.php
+++ src/Traits/HasDump.php
@@ -6,9 +6,6 @@ namespace Manzadey\SBuilderXmlSoap\Traits;
 
 use JetBrains\PhpStorm\NoReturn;
 
-/**
- * @mixin \Manzadey\SBuilderXmlSoap\Category|\Manzadey\SBuilderXmlSoap\Element|\Manzadey\SBuilderXmlSoap\Field|\Manzadey\SBuilderXmlSoap\Plugin|\Manzadey\SBuilderXmlSoap\Plugins
- */
 trait HasDump
 {
     #[NoReturn]
--- src/Traits/HasIsDelete.php
+++ src/Traits/HasIsDelete.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Manzadey\SBuilderXmlSoap\Traits;
 
-/**
- * @mixin \Manzadey\SBuilderXmlSoap\Traits\HasAttribute
- */
 trait HasIsDelete
 {
     /**
